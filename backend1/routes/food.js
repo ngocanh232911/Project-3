04a0e9356c33f65a7f997f0eea65eb1f0f8e59ad
ngocanh232911 <ngocanhle.cmt@gmail.com>
2023-06-07 23:50:03 +0700

    Project

--- backend1/routes/food.js
+++
@@ -1,8 +0,0 @@
-const { foodController } = require("../controllers/foodController");
-
-const router= require("express").Router();
-
-router.post("/" , foodController.addFood);
-module.exports= router;
-
-
